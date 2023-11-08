0774e3b218212b9064bd624dc99d973877ab48c9
Odhran Mahony <mahonyodhran@gmail.com>
2023-11-08 20:58:34 +0000

    added error and log

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"testy.com/greetings"
 )
 
 func main(){
-	message := greetings.Hello("Maggie")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//message := greetings.Hello("Maggie")
 	fmt.Println(message)
 }
